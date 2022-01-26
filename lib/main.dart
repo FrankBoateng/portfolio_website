8a0f5b294dad5ad3a96a2028c131b02d6f589054
Frank Boateng <frankboateng1992@gmail.com>
2022-01-26 03:47:47 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_website/homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,7 +23,28 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+          fontFamily: 'Barlow',
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                  fontSize: 70,
+                  color: Colors.white,
+                  fontFamily: 'DMSerifDisplay'),
+              headline2: TextStyle(
+                  fontSize: 55,
+                  color: Colors.white,
+                  fontFamily: 'DMSerifDisplay'),
+              headline3: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontFamily: 'DMSerifDisplay'),
+              subtitle1: TextStyle(fontSize: 30, color: Colors.grey[500]),
+              subtitle2: TextStyle(fontSize: 20, color: Colors.grey[500]),
+              bodyText1:
+              TextStyle(fontSize: 20, color: Colors.white, height: 1.25),
+              bodyText2:
+              TextStyle(fontSize: 17, color: Colors.white, height: 1.25),
+              caption: TextStyle(fontSize: 15, color: Colors.white, height: 1.25),
+              button: TextStyle(fontSize: 17, color: Color(0xff1e1e24))),
       ),
       home: HomePage(),
     );
